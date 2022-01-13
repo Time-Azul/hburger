ab84276e12da8905c8ae1e2c3b5d73e8276aefea
Inpuro <sopragames.sf@hotmail.com>
2022-01-13 20:47:19 -0300

    create Function AppendChild

--- src/main.ts
+++ src/main.ts
@@ -1,51 +1,57 @@
 import { Ingredient } from "./types/ingredient";
+import appendChild from "./functions/appendChild";
 
 const mainPage = document.querySelector(".mainBurger");
 
 if (mainPage) {
-	let bread: Ingredient[] = [
-		{
-			name: "Pão Tradicional",
-			price: 2,
-		},
-		{
-			name: "Pão Australiano",
-			price: 3,
-		},
-		{
-			name: "Pão de Batata",
-			price: 2.5,
-		},
-	];
+  let bread: Ingredient[] = [
+    {
+      name: "Pão Tradicional",
+      price: 2,
+    },
+    {
+      name: "Pão Australiano",
+      price: 3,
+    },
+    {
+      name: "Pão de Batata",
+      price: 2.5,
+    },
+	{
+		name: "Pão de Frances",
+		price: 10.00,
+	  },
+  ];
 
-	const setBurger = mainPage.querySelector(
-		"section footer button"
-	) as HTMLButtonElement;
-	const countBurger = mainPage.querySelector(
-		"header strong small"
-	) as HTMLElement;
+  const setBurger = mainPage.querySelector(
+    "section footer button"
+  ) as HTMLButtonElement;
+  const countBurger = mainPage.querySelector(
+    "header strong small"
+  ) as HTMLElement;
 
-	const totalPrice = mainPage.querySelector(
-		"footer div.price"
-	) as HTMLDivElement;
+  const totalPrice = mainPage.querySelector(
+    "footer div.price"
+  ) as HTMLDivElement;
 
-	const checkOut = mainPage.querySelector("aside footer button");
+  const checkOut = mainPage.querySelector("aside footer button");
 
-	const breadList = mainPage.querySelector(".bread ul") as HTMLDivElement;
+  const breadList = mainPage.querySelector(".bread ul") as HTMLDivElement;
 
-	breadList.innerHTML = "";
+  breadList.innerHTML = "";
 
-	bread.forEach((item) => {
-		const breadItem = document.createElement("li");
-		breadItem.innerHTML = `
+  bread.forEach((item) => {
+    const breadItem = appendChild(
+      "li",
+      `
 			<label>
 				<input type="radio" name="item" checked />
 				<span></span>
 				<h3>${item.name}</h3>
 				<div>${item.price}</div>
 			</label>
-		`;
-
-		breadList.appendChild(breadItem);
-	});
+		`,
+      breadList
+    );
+  });
 }
