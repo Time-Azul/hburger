edcd94e4954dddcd5af9b85ed5525aac7b6abba7
Inpuro <sopragames.sf@hotmail.com>
2022-01-18 20:14:56 -0300

    corrigindo autenticação na tela login

--- src/auth-login.ts
+++ src/auth-login.ts
@@ -1,4 +1,4 @@
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
 import getFormValues from "./functions/getFormValues";
 
 const pageAuth = document.querySelector("body#auth #login") as HTMLBodyElement;
@@ -9,6 +9,23 @@ if (pageAuth) {
   const form = pageAuth.querySelector("#form-login") as HTMLFormElement;
   const span = form.querySelector("span") as HTMLSpanElement;
   span.style.marginTop = "10px";
+
+ 
+        onAuthStateChanged(getAuth(), () => {
+          if (auth.currentUser) { 
+
+            const url_atual = window.location.href;
+
+            if (url_atual) {
+
+              window.location.assign("index.html");
+
+            }
+            
+          } else {
+            window.location.assign("login.html");
+          }
+        });
 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
