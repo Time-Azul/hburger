6b539f4ed5360c139a287ab1bb08fd3736926ded
Lucas Gomes <lulinux2109@gmail.com>
2022-01-25 20:52:27 -0300

    confirmaçao de pagamento

--- src/pay.ts
+++ src/pay.ts
@@ -2,83 +2,96 @@ import { HTMLInputField } from "./types/HTMLInputField";
 import IMask from "imask";
 import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore";
 import formatCurrency from "./functions/formatCurrency";
-
+import Swal from "sweetalert2";
 
 const pagePay = document.querySelector("section.page") as HTMLElement;
 
 if (pagePay) {
-	const form = pagePay.querySelector("form") as HTMLFormElement;
-	const number = form.querySelector("#number") as HTMLInputField;
-	const validate = form.querySelector("#validate") as HTMLInputField;
-	const code = form.querySelector("#code") as HTMLInputField;
-	const name = form.querySelector("#name") as HTMLInputField;
-	const bank = form.querySelector("#bank") as HTMLInputField;
-	const buttonPay = document.querySelector("#payment") as HTMLButtonElement;
-	const installments = form.querySelector("#installments") as HTMLDivElement;
+  const form = pagePay.querySelector("form") as HTMLFormElement;
+  const number = form.querySelector("#number") as HTMLInputField;
+  const validate = form.querySelector("#validate") as HTMLInputField;
+  const code = form.querySelector("#code") as HTMLInputField;
+  const name = form.querySelector("#name") as HTMLInputField;
+  const bank = form.querySelector("#bank") as HTMLInputField;
+  const buttonPay = document.querySelector("#payment") as HTMLButtonElement;
+  const installments = form.querySelector("#installments") as HTMLDivElement;
 
-	const orderlocalStorage = localStorage.getItem("order");
+  const orderlocalStorage = localStorage.getItem("order");
 
-	if (orderlocalStorage) {
-		
-		const parseOrder = JSON.parse(orderlocalStorage);
+  if (orderlocalStorage) {
+    const parseOrder = JSON.parse(orderlocalStorage);
 
-		const user = parseOrder.user;
-		const order = parseOrder.order;
-		const date = parseOrder.date;
-		const itens = parseOrder.itens;
-		const total = parseOrder.total;
+    const user = parseOrder.user;
+    const order = parseOrder.order;
+    const date = parseOrder.date;
+    const itens = parseOrder.itens;
+    const total = parseOrder.total;
 
-		const calcTotal = total / 2;
-		installments.innerHTML = "";
+    const calcTotal = total / 2;
+    installments.innerHTML = "";
 
-		installments.innerHTML = `
-									<option value="1">1 parcela de ${formatCurrency(total)} (${formatCurrency(total)})</option>
-									<option value="2">2 parcelas de ${formatCurrency(calcTotal)} (${formatCurrency(total)})</option>
+    installments.innerHTML = `
+									<option value="1">1 parcela de ${formatCurrency(total)} (${formatCurrency(
+      total
+    )})</option>
+									<option value="2">2 parcelas de ${formatCurrency(calcTotal)} (${formatCurrency(
+      total
+    )})</option>
 								`;
-				
 
-		buttonPay.addEventListener("click", (e) => {
-		const db = getFirestore();
-		const colletionsOrders = collection(db, 'orders', user);
+    buttonPay.addEventListener("click", (e) => {
+      const db = getFirestore();
+      const colletionsOrders = collection(db, "orders", user);
 
-		
-			addDoc(colletionsOrders, {
-				user,
-				order,
-				date,
-				itens,
-				total
-			})
-		})
-	}
+      addDoc(colletionsOrders, {
+        user,
+        order,
+        date,
+        itens,
+        total,
+      });
+    });
+  }
 
-	number.addEventListener("keyup", (e) => {
-		number.value.replaceAll(" ", "");
-	});
+  number.addEventListener("keyup", (e) => {
+    number.value.replaceAll(" ", "");
+  });
 
-	IMask(number, {
-		mask: "0000 0000 0000 0000",
-	});
+  IMask(number, {
+    mask: "0000 0000 0000 0000",
+  });
 
-	const year = new Date().getFullYear();
+  const year = new Date().getFullYear();
 
-	IMask(validate, {
-		mask: "MM/YY",
-		blocks: {
-			YY: {
-				mask: IMask.MaskedRange,
-				from: year.toString().substring(2, 4),
-				to: (year + 10).toString().substring(2, 4),
-			},
-			MM: {
-				mask: IMask.MaskedRange,
-				from: 1,
-				to: 12,
-			},
-		},
-	});
+  IMask(validate, {
+    mask: "MM/YY",
+    blocks: {
+      YY: {
+        mask: IMask.MaskedRange,
+        from: year.toString().substring(2, 4),
+        to: (year + 10).toString().substring(2, 4),
+      },
+      MM: {
+        mask: IMask.MaskedRange,
+        from: 1,
+        to: 12,
+      },
+    },
+  });
 
-	IMask(code, {
-		mask: "000[0]",
-	});
+  IMask(code, {
+    mask: "000[0]",
+  });
+  
+	buttonPay.addEventListener("click", (e) => {
+    Swal.fire({
+    position: 'center',
+    icon: 'success',
+    title: 'Pagamento Concluído',
+    showConfirmButton: true,
+    timer: 3000
+}), setTimeout(function() {
+	window.location.href = "https://codepen.io";
+    }, 3500);
+  });
 }
